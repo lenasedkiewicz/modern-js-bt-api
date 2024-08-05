25186635211a272c814af41d10193226c282390d
msedkiewicz <magdalenainspirations@gmail.com>
2024-08-05 17:00:43 +0200

    Draw time lines (hour, minutes)

--- 01_AnimatedClock/script.js
+++ 01_AnimatedClock/script.js
@@ -25,6 +25,31 @@ function clock() {
   ctx.fill();
   ctx.restore();
 
+  // Draw hour lines
+  ctx.save();
+  for (let i = 0; i < 12; i++) {
+    ctx.beginPath();
+    ctx.rotate(Math.PI / 6);
+    ctx.moveTo(100, 0);
+    ctx.lineTo(120, 0);
+    ctx.stroke();
+  }
+  ctx.restore();
+
+  // Draw minute lines
+  ctx.save();
+  ctx.lineWidth = 4;
+  for (let i = 0; i < 60; i++) {
+    if (i % 5 !== 0) {
+      ctx.beginPath();
+      ctx.moveTo(117, 0);
+      ctx.lineTo(120, 0);
+      ctx.stroke();
+    }
+    ctx.rotate(Math.PI / 30);
+  }
+  ctx.restore();
+
   ctx.restore(); // restore default state
 }
 
